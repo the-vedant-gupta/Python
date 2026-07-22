640b2ec85e3bb688a311e3fa09183c171c76da21
the-vedant-gupta <vedantgupta8784@gmai.com>
2026-07-22 16:36:42 +0530

    Annotations

---
+++ 16. Annotations/1. basic.py
@@ -0,0 +1,15 @@
+def calculate(a: int, b: int) -> int:
+    return a + b
+
+
+def maxi_num(a: int, b: int, c: int) -> None:
+    print(f"Maximum of a, b,c is {max(a,b,c)}")
+
+
+def greet(name: str, age: int, marks: float, adult: bool):
+    pass
+
+
+print(calculate(23, 4))
+# print(calculate("23", "4"))
+maxi_num(3, 6, 2)
---
+++ 16. Annotations/2. advance.py
@@ -0,0 +1,11 @@
+def max_mark(marks: list[int]) -> int:
+    return max(marks)
+
+
+def print_lst(lst: list[int | str]):
+    print(lst)
+
+
+ans = max_mark([45, 66, 77, 33, 44])
+print(ans)
+print_lst([33, 44, "vedant"])
